b6bbaa3d5ea45982b85a378c170a9ba63de8ee8c
RobDBennett <robbennett.work@gmail.com>
2020-09-29 15:01:27 -0700

    Day 2&3 MVP. Added auto-resize. Corrected delete. Added repr. Added comment notation per line.

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -10,6 +10,9 @@ class HashTableEntry:
         self.value = value
         self.next = None
 
+    def __repr__(self):
+        return f"HashTableEntry({repr(self.key)}, {repr(self.value)})"
+
     def get_key(self):
         return self.key
 
@@ -42,6 +45,8 @@ class HashTable:
         self.array = [None] * capacity
         self.number_of_items = 0
 
+    def __repr__(self):
+        return f"HashTable({repr(self.capacity)}, {repr(len(self.array))}, {repr(self.number_of_items)})"
 
     def get_num_slots(self):
         """
@@ -64,6 +69,7 @@ class HashTable:
         """
         This formula was lifted directly from the pseudo code presented
         on Wikipedia.
+        This is a somewhat black box to me. A large number generator.
         """
         fnv_offset = 14695981039346656037 #offset_basis- from Wikipedia
         fnv_prime = 1099511628211 #FNV_prime- from Wikipedia
@@ -105,24 +111,24 @@ class HashTable:
         Places items into linked_list format.
         Checks for collision 
         """
-        if self.get_load_factor() > .7:
-            self.resize(self.capacity * 2)
+        if self.get_load_factor() > .7: #Checks load factor. If its too high, it resizes the capacity.
+            self.resize(self.capacity * 2) #Standard convention is doubling the size of the hashtable.
 
-        index = self.hash_index(key)
-        entry = HashTableEntry(key, value)
-        self.number_of_items += 1
+        index = self.hash_index(key) #Generates hash 'address'
+        entry = HashTableEntry(key, value) #Creates a node for the given key/value pair
+        self.number_of_items += 1 #Iterates up the number of items
 
-        if self.array[index] == None:
-            self.array[index] = entry
+        if self.array[index] == None: #Checks if the given array 'address' is empty
+            self.array[index] = entry #If so, puts the new node there.
         else:
             current_node = self.array[index]
             while current_node != None:
-                if current_node.get_key() == key:
-                    current_node.set_value(value)
+                if current_node.get_key() == key: #Otherwise it checks to see if the key is already present.
+                    current_node.set_value(value) #If so, it updates the value to the new values.
                     return
-                elif current_node.get_next() == None:
-                    current_node.set_next(entry)
-                current_node = current_node.get_next()
+                elif current_node.get_next() == None: #Checks if the current nodes at address is the 'head'.
+                    current_node.set_next(entry) #If so, adds new node to the end of the node chain
+                current_node = current_node.get_next() #Otherwise, moves down the node-list, checking if the key exists, and adding to the tail.
 
 
     def delete(self, key):
@@ -131,33 +137,30 @@ class HashTable:
         Shifts pointers along the nodes.
         Gives a warning if the key to delete isn't present.
         """
-        #index = self.hash_index(key)
-        #current_node = self.array[index]
-        #prev_entry = None
-        if self.get(key):
-            index = self.hash_index(key)
-            current_node = self.array[index]
-            prev_entry = None 
-        else:
-            print(f"Warning: Tried to delete a value from HashTable but no value exists for key: '{key}'")
+        if self.get(key): #Starts by finding if the key currently exists
+            index = self.hash_index(key) #Set hash 'address'
+            current_node = self.array[index] #Set current node we are looking at to the addressed node
+            prev_entry = None  #Create a second pointer.
+        else: #If key can't be found, raise a warning.
+            return print(f"Warning: Tried to delete a value from HashTable but no value exists for key: '{key}'")
 
-        while current_node != None:
-            if current_node.get_key() == key:
-                self.number_of_items -= 1
-                if prev_entry == None:
-                    current_node.set_value(None)
+        while current_node != None: #Start a loop for as long as there is a current_node pointer
+            if current_node.get_key() == key: #Checks if the current node is the key to delete.
+                self.number_of_items -= 1 #Lowers the items in storage by one.
+                if prev_entry == None: #Checks if the second pointer has moved.
+                    current_node.set_value(None) #Deletes the current node if second pointer hasn't moved.
                 else:
-                    prev_entry.set_next(current_node.get_next())
+                    prev_entry.set_next(current_node.get_next()) #sets second pointer down the track from current pointer.
             
-            prev_entry = current_node
-            current_node = current_node.get_next()
+            prev_entry = current_node #If key wasn't found, set second pointer to first pointer
+            current_node = current_node.get_next() #And first pointer to next linked list item. This will repeat until current_node becomes None.
         
-        if self.get_load_factor() < 0.2:
-            new_capacity = self.capacity // 2
-            if new_capacity < MIN_CAPACITY:
-                self.resize(MIN_CAPACITY)
+        if self.get_load_factor() < 0.2: #After deleting item, checks load size against a 20% full capacity.
+            new_capacity = self.capacity // 2 #If list is less than 20% full, it will set a new capacity as half of the current list
+            if new_capacity < MIN_CAPACITY: #Checks if half of the current list is lower than the minimum capacity.
+                self.resize(MIN_CAPACITY) #If so, resize to minimum capacity.
             else:
-                self.resize(new_capacity)
+                self.resize(new_capacity) #Otherwise, resizes the hashtable to half of the previous capacity.
 
 
     def get(self, key):
@@ -166,14 +169,14 @@ class HashTable:
         If that entry is still None, returns None. Otherwise moves along the 
         linked_list to find entry.
         """
-        index = self.hash_index(key)
-        entry = self.array[index]
-        if entry is None:
+        index = self.hash_index(key) #Generates hash address
+        entry = self.array[index] #Points to the array at hash address
+        if entry is None: #If there isn't a value stored at that address, return None
             return None
-        while entry != None:
-            if entry.get_key() == key:
-                return entry.get_value()
-            entry = entry.get_next()
+        while entry != None: #Otherwise run through the list
+            if entry.get_key() == key: #Check if the key of the current node at that array address is the key.
+                return entry.get_value() #If so, return the value.
+            entry = entry.get_next() #If not, move the current node down a position in the linked list.
         return None
 
 
@@ -182,16 +185,16 @@ class HashTable:
         Creates an object for the old array. Resets array to a new capacity.
         Runs through the old array to input values into new array.
         """
-        old_array = self.array
-        self.array = [None] * new_capacity
-        self.capacity = new_capacity
+        old_array = self.array #Move the data currently stored in the hashtable into a new variable
+        self.array = [None] * new_capacity #Reset everything previously stored in the table to a number of buckets equal to the new capacity
+        self.capacity = new_capacity #Set capacity to the new capacity
+        self.number_of_items = 0 #Reset the counter to 0
 
-        for index in range(len(old_array)):
-            current_node = old_array[index]
-            while current_node != None:
+        for index in range(len(old_array)): #Iterate over the storage array 
+            current_node = old_array[index] #Set the pointer to the first point in that array
+            while current_node != None: #If that node isn't empty, 'put' that key/value pair into a new node via 'put'
                 self.put(current_node.get_key(), current_node.get_value())
-                current_node = current_node.get_next()
-
+                current_node = current_node.get_next() #Shift pointer down node.
 
 
 if __name__ == "__main__":
